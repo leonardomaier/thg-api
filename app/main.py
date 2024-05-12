61ef462a086803343b6f1cd7d0e76d491f7c1fec
Leonardo Maier <leonarmaier@gmail.com>
2024-05-12 18:22:03 -0300

    adds error handling if external api fails

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import requests
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from urllib.parse import urlencode
 from .utils import extract_population_by_year
@@ -22,44 +22,52 @@ async def home():
 
 @app.get("/population")
 async def get_population():
-    params = {
-        'drilldowns': 'State',
-        'measures': 'Population',
-        'geo': '04000US01,04000US12,04000US06'
-    }
+    try:
+        params = {
+            'drilldowns': 'State',
+            'measures': 'Population',
+            'geo': '04000US01,04000US12,04000US06'
+        }
 
-    query_string = urlencode(params)
+        query_string = urlencode(params)
 
-    response = requests.get(f'{API_URL}?{query_string}')
+        response = requests.get(f'{API_URL}?{query_string}')
+        response.raise_for_status()
 
-    json = response.json()
+        json = response.json()
 
-    result = extract_population_by_year(json["data"])
+        result = extract_population_by_year(json["data"])
 
-    return { 'data': result }
+        return { 'data': result }
+    except requests.RequestException as e:
+        raise HTTPException(status_code=503, detail="Error fetching data from external API")
 
 @app.get("/cars-per-household")
 async def get_cars_per_household():
-    params = {
-        'measure': 'Commute Means by Gender,Commute Means by Gender Moe',
-        'geo': '01000US,01000US',
-        'drilldowns': 'Vehicles Available',
-        'year': 2021
-    }
+    try:
+        params = {
+            'measure': 'Commute Means by Gender,Commute Means by Gender Moe',
+            'geo': '01000US,01000US',
+            'drilldowns': 'Vehicles Available',
+            'year': 2021
+        }
 
-    query_string = urlencode(params)
+        query_string = urlencode(params)
 
-    response = requests.get(f'{API_URL}?{query_string}')
+        response = requests.get(f'{API_URL}?{query_string}')
+        response.raise_for_status()
 
-    json = response.json()
+        json = response.json()
 
-    result = { 'labels': [], 'quantity': [] }
+        result = { 'labels': [], 'quantity': [] }
 
-    for row in json['data']:
-        vehicles_avaiable = row['Vehicles Available']
-        vehicles_quantity = row['Commute Means by Gender']
+        for row in json['data']:
+            vehicles_avaiable = row['Vehicles Available']
+            vehicles_quantity = row['Commute Means by Gender']
 
-        result['labels'].append(vehicles_avaiable)
-        result['quantity'].append(vehicles_quantity)
+            result['labels'].append(vehicles_avaiable)
+            result['quantity'].append(vehicles_quantity)
 
-    return { "data": result }
+        return { "data": result }
+    except requests.RequestException as e:
+            raise HTTPException(status_code=503, detail="Error fetching data from external API")
